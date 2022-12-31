564b2c410a439c042cb45a261650b7b559bc199c
2022phyro <phyrokelstein2@gmail.com>
2022-12-31 20:33:04 +0100

    recognize input lines in the files

--- utility1.c
+++ utility1.c
@@ -56,7 +56,7 @@ bool appendStr(char ***arr, size_t *size, char *str, int index)
  * - delimiters is empty
  */
 
-char **split(char *string, char *delimiter, unsigned int max)
+char **split(char *string, char *delimiter)
 {
 	char **array, prev = '\0', curr;
 	size_t arr_size = 2;
@@ -76,6 +76,8 @@ char **split(char *string, char *delimiter, unsigned int max)
 	for (str_ind = 0; string[str_ind]; str_ind++)
 	{
 		curr = string[str_ind];
+		if ((curr == '\n' && (string[str_ind - 1] == '\0' || string[str_ind -1 ] == '\n')) && strcmp(delimiter,"\n") == 0)
+			appendStr(&array, &arr_size, "#", arr_ind++);
 		flipped = false;
 		for (i = 0; delimiter[i]; i++)
 			if (curr == delimiter[i])
@@ -86,8 +88,6 @@ char **split(char *string, char *delimiter, unsigned int max)
 			}
 		if (prev == '\0' && flipped == false)
 			appendStr(&array, &arr_size, &string[str_ind], arr_ind++);
-		if (max && max + 1 == arr_ind)
-			break;
 		prev = string[str_ind];
 	}
 	if (arr_ind)
